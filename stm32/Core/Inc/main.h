1f3ff9e9f2a2bab6c790f2c505c485fc0e8eec9e
Tage Kristensen <tage.kristensen@2-0.dk>
2020-11-19 14:06:20 +0100

    created initial kicad and stm32 projects

---
+++ stm32/Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define HMI_ERROR2_Pin GPIO_PIN_14
+#define HMI_ERROR2_GPIO_Port GPIOC
+#define HMI_STATUS2_Pin GPIO_PIN_15
+#define HMI_STATUS2_GPIO_Port GPIOC
+#define E22_DIO1_Pin GPIO_PIN_2
+#define E22_DIO1_GPIO_Port GPIOA
+#define E22_BUSY_Pin GPIO_PIN_3
+#define E22_BUSY_GPIO_Port GPIOA
+#define E22_NRST_Pin GPIO_PIN_4
+#define E22_NRST_GPIO_Port GPIOA
+#define E22_DIO2_Pin GPIO_PIN_0
+#define E22_DIO2_GPIO_Port GPIOB
+#define E22_TXEN_Pin GPIO_PIN_1
+#define E22_TXEN_GPIO_Port GPIOB
+#define E22_RXEN_Pin GPIO_PIN_2
+#define E22_RXEN_GPIO_Port GPIOB
+#define E22_CS_Pin GPIO_PIN_12
+#define E22_CS_GPIO_Port GPIOB
+#define HMI_ERROR1_Pin GPIO_PIN_8
+#define HMI_ERROR1_GPIO_Port GPIOB
+#define HMI_STATUS1_Pin GPIO_PIN_9
+#define HMI_STATUS1_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
